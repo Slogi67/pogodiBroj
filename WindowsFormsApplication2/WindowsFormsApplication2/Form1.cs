595f507e4ab746b18e67c62cb724efd774e6912b
Slogi67 <jaksa.glisic@gmail.com>
2026-05-20 14:21:06 +0200

    Add files via upload

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        int broj = 7;
+
+        public Form1()
+        {
+            InitializeComponent();
+            label2.Text = "0";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text == broj.ToString())
+            {
+                label1.Text = "true";
+            }
+            else
+            {
+                label1.Text = "false";
+                if(textBox1.Text!="")
+                    label2.Text = (int.Parse(label2.Text) + 1).ToString();
+            }
+        }
+
+
+
+    }
+}
